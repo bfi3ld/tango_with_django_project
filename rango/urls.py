ab2cefcb7fc268a90bf96e6cc8d3a15758df1527
Hanna Baekkevold <2419965b@student.gla.ac.uk>
2019-01-28 00:09:09 +0000

    generic reformatting

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from rango import views
 
-urlpatterns = [ url(r'^$',views.index, name='index'),
-
-                ]
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
