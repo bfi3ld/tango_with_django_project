3f04ea4ea5c8f89296a023939facab238a1b0a62
Hanna Baekkevold <2419965b@student.gla.ac.uk>
2019-01-28 19:30:39 +0000

    updating

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+
+
 
 def index(request):
-    return HttpResponse("lol")
+    return HttpResponse("Rango says hey there partner",
+    <a>href= '/rango/about/'>About</a>,)
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page")
